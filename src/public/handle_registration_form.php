88451ca5e2700b20a40d751deb40120fab1a9a38
rabdanova <rabdanova.dimit@mail.ru>
2025-04-18 21:16:23 +0700

    created a registration form

--- src/public/handle_registration_form.php
+++ src/public/handle_registration_form.php
@@ -1,12 +1,43 @@
 <?php
 
-print_r($_GET);
+$name = $_GET['username'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+$repeat_password = $_GET['repeat-pas'];
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$repeat_password = $_POST['repeat_password'];
+$flag = true;
 
-$pdo = new PDO('pgsql:host=postgres; port = 5432;dbname=mydb', 'user', 'pass');
+if ((strlen($name) <= 3)) {
+    echo $name;
+    $flag = false;
+    echo 'недопустимая длина имени'."\n";
+}
 
-$pdo->exec("INSERT INTO users (name, email, password) VALUES ('Ivan','ttt@mail.ru','asaa')");
+if (strlen($email) <= 4) {
+    echo $email;
+    $flag = false;
+    echo 'Слишком короткий почтовый адрес'."\n";
+}
+
+if (strlen($password) <= 5) {
+    echo $password;
+    $flag = false;
+    echo 'недопустимая длина пароля. Пожалуйста, придумайте более длинный пароль!'."\n";
+}
+
+if ($repeat_password !== $password) {
+    echo $repeat_password;
+    $flag = false;
+    echo 'пароли не совпадают'."\n";
+}
+
+if ($flag === true){
+    $pdo = new PDO('pgsql:host=postgres; port = 5432;dbname=mydb', 'user', 'pass');
+
+    $pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name','$email','$password')");
+    $result=$pdo->query("select * from users order by id desc limit 1");
+    $data = $result->fetch();
+    print_r($data);
+
+}
+
