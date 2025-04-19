5b701980dfdfdca2001fa943b99537d8881a1b6d
rabdanova <rabdanova.dimit@mail.ru>
2025-04-19 17:45:55 +0700

    add a function IsValidData

--- src/public/handle_registration_form.php
+++ src/public/handle_registration_form.php
@@ -5,39 +5,41 @@ $email = $_GET['email'];
 $password = $_GET['password'];
 $repeat_password = $_GET['repeat-pas'];
 
-$flag = true;
+function IsValidData($name,$email,$password,$repeat_password)
+{
+    $flag = true;
 
-if ((strlen($name) <= 3)) {
-    echo $name;
-    $flag = false;
-    echo 'недопустимая длина имени'."\n";
+    if ((strlen($name) <= 3)) {
+        $flag = false;
+        echo 'Недопустимая длина имени' . "\n";
+    }
+
+    if (strlen($email) <= 4) {
+        $flag = false;
+        echo 'Слишком короткий почтовый адрес' . "\n";
+    }
+
+    if (strlen($password) <= 5) {
+        $flag = false;
+        echo 'Недопустимая длина пароля. Пожалуйста, придумайте более длинный пароль!' . "\n";
+    }
+
+    if ($repeat_password !== $password) {
+        $flag = false;
+        echo 'Пароли не совпадают' . "\n";
+    }
+
+    if ($flag === true) {
+        $pdo = new PDO('pgsql:host=postgres; port = 5432;dbname=mydb', 'user', 'pass');
+
+        $pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name','$email','$password')");
+
+        $result = $pdo->query("select * from users order by id desc limit 1");
+
+        $data = $result->fetch();
+
+        print_r($data);
+    }
 }
 
-if (strlen($email) <= 4) {
-    echo $email;
-    $flag = false;
-    echo 'Слишком короткий почтовый адрес'."\n";
-}
-
-if (strlen($password) <= 5) {
-    echo $password;
-    $flag = false;
-    echo 'недопустимая длина пароля. Пожалуйста, придумайте более длинный пароль!'."\n";
-}
-
-if ($repeat_password !== $password) {
-    echo $repeat_password;
-    $flag = false;
-    echo 'пароли не совпадают'."\n";
-}
-
-if ($flag === true){
-    $pdo = new PDO('pgsql:host=postgres; port = 5432;dbname=mydb', 'user', 'pass');
-
-    $pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name','$email','$password')");
-    $result=$pdo->query("select * from users order by id desc limit 1");
-    $data = $result->fetch();
-    print_r($data);
-
-}
-
+IsValidData($name,$email,$password,$repeat_password);
