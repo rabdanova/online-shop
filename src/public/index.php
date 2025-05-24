ebf81042567f68a86b8559f44d1bae6bba0585f3
rabdanova <rabdanova.dimit@mail.ru>
2025-05-24 17:29:04 +0700

    делает общий автозагрузчик и реализацию страницы оформления заказа

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,18 @@
 <?php
 
+$autoload = function (string $className) {
+    $path = './../' . str_replace('\\', '/', $className) . '.php';
+
+    if (file_exists($path)) {
+        require_once $path;
+        return true;
+    }
+
+    return false;
+};
+
+spl_autoload_register($autoload);
 require_once '../Core/App.php';
 
-$app = new App();
+$app = new Core\App();
 $app->run();
